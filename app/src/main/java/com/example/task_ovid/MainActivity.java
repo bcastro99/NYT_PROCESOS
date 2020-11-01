693ee9dc5cc2088b510d315e9e0cac744dee1eee
miri-fa <miriamdefco@gmail.com>
2020-11-01 12:59:37 +0100

    prueba con el main

--- app/src/main/java/com/example/task_ovid/MainActivity.java
+++ app/src/main/java/com/example/task_ovid/MainActivity.java
@@ -10,18 +10,11 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView) findViewById(R.id.listview);
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("Lavarse las manos");
-        arrayList.add("ponerse la mascarilla");
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
-        listView.setAdapter(arrayAdapter);
 
     }
 }
