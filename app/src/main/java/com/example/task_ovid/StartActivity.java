3d6190c3ba116c57f2a400b70a4c5dbb749cd5b5
Yeray.C <y.cornejo.2018@alumnos.urjc.es>
2020-11-02 14:34:30 +0100

    Actualizacion Boton

---
+++ app/src/main/java/com/example/task_ovid/StartActivity.java
@@ -0,0 +1,32 @@
+package com.example.task_ovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button stbut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        stbut=(Button)findViewById(R.id.button_start);
+        stbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Startup();
+            }
+        });
+    }
+
+    public void Startup(){ // Metodo para que el boton comunique con la otra pantalla
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
