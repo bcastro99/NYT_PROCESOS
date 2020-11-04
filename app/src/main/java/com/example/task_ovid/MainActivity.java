567b14c2361ff98b38602ed2dc03aaa2c2b05e19
miri-fa <miriamdefco@gmail.com>
2020-11-04 19:14:44 +0100

    listas implementacion, 4/10/2020

--- app/src/main/java/com/example/task_ovid/MainActivity.java
+++ app/src/main/java/com/example/task_ovid/MainActivity.java
@@ -1,23 +1,62 @@
 package com.example.task_ovid;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.text.DateFormatSymbols;
 import java.util.ArrayList;
 import java.util.zip.DataFormatException;
 
 
-public class MainActivity extends AppCompatActivity {
-    ListView lista;
-    String[] tareas;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ListView lista;
+    private ArrayList<String> tareas;
+    ArrayAdapter<String> tareasAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pantallaprincipal);
+        lista = findViewById(R.id.lista);
+        this.tareas = new ArrayList<>();
+        llenarTareas();
+        tareasAdapter = new ArrayAdapter(this, R.layout.rowtext, tareas);
+        lista.setAdapter(tareasAdapter);
+        lista.setOnItemClickListener(this);
+    }
+    private void llenarTareas(){
+        tareas.add("salir con mascarilla 0");
+        tareas.add("olvidarse la mascarilla 0");
+        tareas.add("lavarse las manos 0");
+        tareas.add("desinfectarse 0");
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String t = parent.getItemAtPosition(position).toString();
+        String[] parts = t.split(" ");
+
+        int cont = Integer.parseInt(parts[parts.length-1]) + 1;
+        parts[parts.length-1] = String.valueOf(cont);
+
+        String string = "";
+        for (int i = 0; i <= parts.length-1; i++){
+            if (i == 0){
+                string = parts[i];
+            }else{
+                string = string + " "+parts[i];
+            }
+        }
+
+        tareas.set(position,string);
+        tareasAdapter.notifyDataSetChanged();
     }
 }
