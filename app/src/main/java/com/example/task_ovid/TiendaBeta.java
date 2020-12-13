17c080533e596d7613234465ce5e721d7767194d
Yeray <y.cornejo.2018@alumnos.urjc.es>
2020-12-13 14:02:01 +0100

    Act 1.0

--- app/src/main/java/com/example/task_ovid/TiendaBeta.java
+++ app/src/main/java/com/example/task_ovid/TiendaBeta.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +16,32 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TiendaBeta extends AppCompatActivity {
+
+    private ImageButton p1;
+    private ImageButton p2;
+    private ImageButton m1;
+    private ImageButton m2;
+    private int vidaAux;
+    private int expAux;
+    private MainActivity x;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tienda_beta);
+
+        p1=(ImageButton)findViewById(R.id.pocion1);
+        p2=(ImageButton)findViewById(R.id.pocion2);
+        m1=(ImageButton)findViewById(R.id.masc1);
+        m2=(ImageButton)findViewById(R.id.masc2);
+    }
+
+    public void generar(){
+        x=new MainActivity();
+        this.vidaAux = x.getVida();
+
+    }
+
 
 
 }
